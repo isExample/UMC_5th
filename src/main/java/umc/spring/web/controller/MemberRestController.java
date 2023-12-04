376196f01091e316f07b430964890ec980d08af5
isExample <yeji0331@naver.com>
2023-12-05 01:47:59 +0900

    [FEAT] 미션 도전하기 API #12

--- src/main/java/umc/spring/web/controller/MemberRestController.java
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,5 +1,6 @@
 package umc.spring.web.controller;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
+import umc.spring.converter.MemberMissionConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.service.memberService.MemberCommandService;
 import umc.spring.web.dto.MemberRequestDTO;
 import umc.spring.web.dto.MemberResponseDTO;
@@ -25,5 +28,11 @@ public class MemberRestController {
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @PostMapping("/missions/")
+    public ApiResponse<MemberResponseDTO.ChallengeDTO> challenge(@RequestBody @Valid MemberRequestDTO.ChallengeDto request){
+        MemberMission memberMission = memberCommandService.challengeMission(request);
+        return ApiResponse.onSuccess(MemberMissionConverter.toChallengeDto(memberMission));
     }
 }
